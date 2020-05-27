e4fc7a3859d77cb673104c223e4e7f3ee9e440ba
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-05-27 04:18:41 +0400

    add home page without responsive

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 $('.home_info_slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: true,
+  arrows: false,
   fade: false,
   speed:2000,
   autoplay: true,
@@ -14,28 +14,19 @@ $('.home_info_slider').slick({
 });
 
 
-
-
-
-	var vids = $(".home_video_box video"); 
+var vids = $(".home_video_box video"); 
 	$.each(vids, function(){
 	       this.controls = false; 
 	}); 
-
     $(".home_video_box video").click(function() {
 	  if (this.paused) {
 	    this.play();
 	  } else {
 	    this.pause();
 	  }
-
  	 });
-
-
 	$(".home_video_box").click(function() {
-
 		$(this).find(".video_decor").toggleClass("active");
-		
 	})
 
 })
